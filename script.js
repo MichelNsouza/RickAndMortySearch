b1c767e5d32e1502e4179aa9238e623725cf95df
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-04-17 15:24:36 +0000

    finalizando dropdown de episodios

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-const app = () =>{
+const app = () => {
   return {
-    dadosDosPersonas:[], 
+    dadosDosPersonas: [],
     personagemSelecionado: null,
-    dadosDosPersonaEpisodios: [],
+    episodiosSelecionado: [],
     urlPadrao: "https://rickandmortyapi.com/api/character/avatar/19.jpeg",
     init() {
-        axios.get('https://rickandmortyapi.com/api/character')
+      axios.get('https://rickandmortyapi.com/api/character')
         .then((resposta) => {
-          this.dadosDosPersonas = resposta.data.results
+          this.dadosDosPersonas = resposta.data.results;
           console.log(this.dadosDosPersonas);
         })
         .catch((error) => {
           console.log(error);
         })
-      },
+    },
     selecionarPersonagem(personagem) {
       this.personagemSelecionado = personagem;
-
-      this.dadosDosPersonaEpisodios = [];
-
-      for(episodio in personagem.episode){
-        
-        axios.get(episodio)
-        .then((resposta) => {
-          this.dadosDosPersonaEpisodios.push(resposta.data.name);
-          console.log(resposta.data.name);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-      }
       console.log(this.personagemSelecionado);
-      
-       console.log(this.dadosDosPersonaEpisodios);
+      thi = [];
+      let episodios = personagem.episode;
+      console.log(episodios);
+
+      for (episodio of episodios) {
+        axios.get(episodio)
+          .then((resposta) => {
+            this.episodiosSelecionado.push("Ep:"+resposta.data.id+" - "+resposta.data.name);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+      console.log(this.episodiosSelecionado);
     }
   }
 }
+
